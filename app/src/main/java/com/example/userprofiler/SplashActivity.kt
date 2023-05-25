cfebc2f5a5e3a33f435a302fc37f3b9fc568e135
HyoenGyuJang <godkad@naver.com>
2023-05-24 22:47:27 -0700

    Google 로그인 기능 추가

--- app/src/main/java/com/example/userprofiler/SplashActivity.kt
+++ app/src/main/java/com/example/userprofiler/SplashActivity.kt
@@ -1,8 +1,9 @@
 package com.mrkimkim.userprofiler
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -14,22 +15,59 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.mrkimkim.userprofiler.ui.theme.UserProfilerTheme
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : ComponentActivity() {
+    private val RC_SIGN_IN = 1000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { SplashScreen() }
+        googleSignIn()
+    }
+
+    private fun googleSignIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso);
+        val account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account == null) {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        } else {
+            navigateToMainActivity()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                task.getResult(ApiException::class.java)
+                navigateToMainActivity()
+            } catch (e: ApiException) {
+                Log.e("SplashActivity", "signInResult:failed code=" + e.message)
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun navigateToMainActivity() {
         GlobalScope.launch {
-            delay(3000)  // 3초 동안 Splash screen 보여줌
+            delay(2000)  // 3초 동안 Splash screen 보여줌
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }
     }
-
+    
     @Composable
     fun SplashScreen() {
         UserProfilerTheme {
